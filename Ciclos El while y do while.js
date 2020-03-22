e082926660ebc077a225ed2f603849621710071f
Martin Espinosa <martinespinosa77@gmail.com>
2020-03-22 20:35:00 -0300

    aca subo mis tres archivos

---
+++ Ciclos El while y do while.js
@@ -0,0 +1,8 @@
+function encontreUn5(numeros){
+    let i = -1;
+    do{
+            i++;
+            console.log(numeros[i]);
+    }while(numeros[i] != 5);
+    console.log("Se encontró un 5!");
+}
